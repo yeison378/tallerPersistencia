1649e305cb6f01069a896ecbabce03a44a6eba53
yeison378 <yeisonalexis1@outlook.es>
2022-03-20 22:33:02 -0500

    huella

--- src/main/java/com/example/tallerpersistencia/Celular.java
+++ src/main/java/com/example/tallerpersistencia/Celular.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.tallerpersistencia;
 
 public class Celular {
 
--- src/main/java/com/example/tallerpersistencia/ControllerCreate.java
+++ src/main/java/com/example/tallerpersistencia/ControllerCreate.java
@@ -11,30 +11,13 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import model.Celular;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerCreate implements Initializable {
 
-    ObservableList<Celular> list = FXCollections.observableArrayList(
-            new Celular("1","s22","android","22 Mpx","16 mpx","6 gb","256","si")
-    );
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        colModelo.setCellValueFactory(new PropertyValueFactory<Celular,String>("modelo"));
-        colCamPrincipal.setCellValueFactory(new PropertyValueFactory<Celular,String>("camaraPrincipal"));
-        colCamFrontal.setCellValueFactory(new PropertyValueFactory<Celular,String>("camaraFrontal"));
-        colOs.setCellValueFactory(new PropertyValueFactory<Celular,String>("sistemaOperativo"));
-        colRam.setCellValueFactory(new PropertyValueFactory<Celular,String>("ram"));
-        colAlmacenamiento.setCellValueFactory(new PropertyValueFactory<Celular,String>("almacenamiento"));
-        colHuella.setCellValueFactory(new PropertyValueFactory<Celular,String>("huella"));
-        colId.setCellValueFactory(new PropertyValueFactory<Celular,String>("id"));
-        System.out.println("error");
-        tabla.setItems(list);
-    }
 
     @FXML
     private TextField almacenamiento,camFrontal,camPrincipal,huella,modelo,os,ram;
@@ -69,10 +52,28 @@ public class ControllerCreate implements Initializable {
     @FXML
     private TableColumn<Celular, String> colRam;
 
+    ObservableList<Celular> list = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colModelo.setCellValueFactory(new PropertyValueFactory<Celular,String>("modelo"));
+        colCamPrincipal.setCellValueFactory(new PropertyValueFactory<Celular,String>("camaraPrincipal"));
+        colCamFrontal.setCellValueFactory(new PropertyValueFactory<Celular,String>("camaraFrontal"));
+        colOs.setCellValueFactory(new PropertyValueFactory<Celular,String>("sistemaOperativo"));
+        colRam.setCellValueFactory(new PropertyValueFactory<Celular,String>("ram"));
+        colAlmacenamiento.setCellValueFactory(new PropertyValueFactory<Celular,String>("almacenamiento"));
+        colHuella.setCellValueFactory(new PropertyValueFactory<Celular,String>("huella"));
+        colId.setCellValueFactory(new PropertyValueFactory<Celular,String>("id"));
+        System.out.println("error");
+        tabla.setItems(list);
+    }
+
 
     @FXML
     void eventAgregar(ActionEvent event) {
-
+        Celular celular = new Celular("1",modelo.getText(),os.getText(),camPrincipal.getText(),camFrontal.getText(),ram.getText(),almacenamiento.getText(),huella.getText());
+        list.add(celular);
+        tabla.setItems(list);
     }
 
     @FXML
